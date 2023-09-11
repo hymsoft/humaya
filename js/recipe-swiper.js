d1b78229fe28c6dbffa674b7851cf3d54ae9d50b
hymsoft <hymsoft@gmail.com>
2023-09-11 06:12:15 -0300

    Agregado el footer

---
+++ js/recipe-swiper.js
@@ -0,0 +1,34 @@
+{/* <script> */ }
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+    },
+    speed: 1000,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+    },
+});
+{/* </script> */ }
